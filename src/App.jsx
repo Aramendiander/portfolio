3da94c6fa49ebcc2bd2262a8fb5eff2b8afebd51
Aramendiander <aramendiander@gmail.com>
2023-12-02 18:07:34 +0100

    added mobile scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,65 @@ import './App.css';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
   const pagesRef = useRef([]);
+
+
+  useEffect(() => {
+    let startY;
+    const threshold = 100;
+  
+    const handleTouchStart = (event) => {
+      startY = event.touches[0].clientY;
+    };
+  
+    const handleTouchMove = (event) => {
+      if (isScrolling) return;
+  
+      const endY = event.touches[0].clientY;
+      const deltaY = startY - endY;
+  
+      if (Math.abs(deltaY) > threshold) {
+        setIsScrolling(true);
+        if (deltaY > 0 && currentPage < pagesRef.current.length - 1) {
+          setCurrentPage(currentPage + 1);
+        } else if (deltaY < 0 && currentPage > 0) {
+          setCurrentPage(currentPage - 1);
+        }
+        setTimeout(() => setIsScrolling(false), 1000); // Adjust this value to change the delay
+      }
+    };
+  
+    window.addEventListener('touchstart', handleTouchStart);
+    window.addEventListener('touchmove', handleTouchMove);
+  
+    return () => {
+      window.removeEventListener('touchstart', handleTouchStart);
+      window.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, [currentPage, isScrolling]);
+
+
 
   useEffect(() => {
     const handleWheel = (event) => {
+      if (isScrolling) return;
+  
+      setIsScrolling(true);
       if (event.deltaY > 0 && currentPage < pagesRef.current.length - 1) {
         setCurrentPage(currentPage + 1);
       } else if (event.deltaY < 0 && currentPage > 0) {
         setCurrentPage(currentPage - 1);
       }
+      setTimeout(() => setIsScrolling(false), 1000);
     };
-
+  
     window.addEventListener('wheel', handleWheel);
-
+  
     return () => {
       window.removeEventListener('wheel', handleWheel);
     };
-  }, [currentPage]);
+  }, [currentPage, isScrolling]);
 
   useEffect(() => {
     scrollToPage(currentPage);
